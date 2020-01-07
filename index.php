8b6e63d05ec1bf0c166debb2b5b212b7b38afeef
Clément LAURENCE <c.laurencepro@gmail.com>
2020-01-07 09:30:03 +0100

    Premiere version

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Méthode POST</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
+    </script>
+    
+</head>
+
+
+
+<body>
+        <div class="container">
+
+                <h1 class="display-4 text-center text-secondary">Jack & Mitchel - The new generation -</h1><hr>
+
+                <p class="alert alert-info text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dignissimos quasi quaerat adipisci iure eum earum, cumque reiciendis, et architecto laboriosam a atque doloremque ad delectus. Iste dolores quaerat id.</p>
+                
+
+        </div>
+
+</body>
+
+</html>
